2c2b2a6509faa9d3d1a7c5db27a57ca1f4f5aed2
Kim woo jong <skekdnwhd@gmail.com>
2023-05-14 02:47:36 +0900

    Feat : 글 조회시 글 등록일 response에 추가

--- src/main/java/com/hello/borad/dto/response/PostResponse.java
+++ src/main/java/com/hello/borad/dto/response/PostResponse.java
@@ -2,9 +2,23 @@ package com.hello.borad.dto.response;
 
 import com.hello.borad.domain.board.entity.Post;
 
-public record PostResponse(Long postId, String title, String content, CategoryResponse categoryResponse) {
+import java.time.format.DateTimeFormatter;
+
+public record PostResponse(
+        Long postId,
+        String title,
+        String content,
+        String createdAt,
+        CategoryResponse categoryResponse) {
+
+    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     public static PostResponse from(Post post) {
-        return new PostResponse(post.getId(), post.getTitle(), post.getContent(), CategoryResponse.from(post.getCategory()));
+        return new PostResponse(
+                post.getId(),
+                post.getTitle(),
+                post.getContent(),
+                post.getCreatedAt().format(DATE_FORMAT),
+                CategoryResponse.from(post.getCategory()));
     }
 }
