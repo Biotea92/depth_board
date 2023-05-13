db5d925f3584a4b1b817f729efd6c8848490e022
Kim woo jong <skekdnwhd@gmail.com>
2023-05-13 16:23:27 +0900

    Test : 카테고리 조회 기능 API 통합테스트, API Docs 추가

--- src/test/java/com/hello/borad/application/presentation/board/BoardControllerDocsTest.java
+++ src/test/java/com/hello/borad/application/presentation/board/BoardControllerDocsTest.java
@@ -15,19 +15,25 @@ import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
+
+import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.modifyHeaders;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @ExtendWith(RestDocumentationExtension.class)
+@Transactional
 class BoardControllerDocsTest {
 
     MockMvc mockMvc;
@@ -59,6 +65,7 @@ class BoardControllerDocsTest {
         CategoryCreateRequest request = new CategoryCreateRequest("카테고리1", 1L);
         String json = objectMapper.writeValueAsString(request);
 
+        // expected
         mockMvc.perform(post("/api/board/category")
                         .contentType(APPLICATION_JSON)
                         .content(json))
@@ -73,6 +80,54 @@ class BoardControllerDocsTest {
                                 fieldWithPath("title").description("카테고리 제목"),
                                 fieldWithPath("depth").description("카테고리 depth"),
                                 fieldWithPath("sequence").description("카테고리 순서")
+                        )
+                ));
+    }
+
+    @Test
+    @DisplayName("카테고리를 조회한다.")
+    void getCategories() throws Exception {
+        // given
+        Category parentCategory1 = CategoryFixtureFactory.create("카테고리 대분류1", 1, 1);
+        Category parentCategory2 = CategoryFixtureFactory.create("카테고리 대분류2", 1, 2);
+        Category parentCategory3 = CategoryFixtureFactory.create("카테고리 대분류3", 1, 3);
+        categoryRepository.saveAll(List.of(parentCategory1, parentCategory2, parentCategory3));
+
+        Category childCategory1_1 = CategoryFixtureFactory.create("카테고리 1-1", 2, 1, parentCategory1);
+        Category childCategory1_2 = CategoryFixtureFactory.create("카테고리 1-2", 2, 2, parentCategory1);
+        Category childCategory1_3 = CategoryFixtureFactory.create("카테고리 1-3", 2, 3, parentCategory1);
+        categoryRepository.saveAll(List.of(childCategory1_1, childCategory1_2, childCategory1_3));
+        parentCategory1.getChildCategories().addAll(List.of(childCategory1_1, childCategory1_2, childCategory1_3));
+
+        Category childCategory2_1 = CategoryFixtureFactory.create("카테고리 2-1", 2, 1, parentCategory2);
+        Category childCategory2_2 = CategoryFixtureFactory.create("카테고리 2-2", 2, 2, parentCategory2);
+        Category childCategory2_3 = CategoryFixtureFactory.create("카테고리 2-3", 2, 3, parentCategory2);
+        categoryRepository.saveAll(List.of(childCategory2_1, childCategory2_2, childCategory2_3));
+        parentCategory2.getChildCategories().addAll(List.of(childCategory2_1, childCategory2_2, childCategory2_3));
+
+        Category childCategory3_1 = CategoryFixtureFactory.create("카테고리 3-1", 2, 1, parentCategory3);
+        Category childCategory3_2 = CategoryFixtureFactory.create("카테고리 3-2", 2, 2, parentCategory3);
+        Category childCategory3_3 = CategoryFixtureFactory.create("카테고리 3-3", 2, 3, parentCategory3);
+        categoryRepository.saveAll(List.of(childCategory3_1, childCategory3_2, childCategory3_3));
+        parentCategory3.getChildCategories().addAll(List.of(childCategory3_1, childCategory3_2, childCategory3_3));
+
+        // expected
+        mockMvc.perform(get("/api/board/category"))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andDo(document(DOCUMENT_IDENTIFIER,
+                        responseFields(
+                                fieldWithPath("[].categoryId").description("대분류 category id"),
+                                fieldWithPath("[].title").description("대분류 category title"),
+                                fieldWithPath("[].depth").description("대분류 깊이"),
+                                fieldWithPath("[].sequence").description("대분류 순서"),
+                                fieldWithPath("[].hasPost").description("대분류의 게시글 여부"),
+                                fieldWithPath("[].childCategoryResponses").description("자식 카테고리"),
+                                fieldWithPath("[].childCategoryResponses[].categoryId").description("소분류 category id"),
+                                fieldWithPath("[].childCategoryResponses[].title").description("소분류 category title"),
+                                fieldWithPath("[].childCategoryResponses[].depth").description("소분류 깊이"),
+                                fieldWithPath("[].childCategoryResponses[].sequence").description("소분류 순서"),
+                                fieldWithPath("[].childCategoryResponses[].hasPost").description("소분류 게시글 여부")
                         )
                 ));
     }
--- src/test/java/com/hello/borad/utils/fixture/CategoryFixtureFactory.java
+++ src/test/java/com/hello/borad/utils/fixture/CategoryFixtureFactory.java
@@ -4,6 +4,7 @@ import com.hello.borad.domain.board.entity.Category;
 import org.jeasy.random.EasyRandom;
 import org.jeasy.random.EasyRandomParameters;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.jeasy.random.FieldPredicates.*;
@@ -45,8 +46,49 @@ public class CategoryFixtureFactory {
                 .randomize(depthPredicate, () -> depth)
                 .randomize(sequencePredicate, () -> sequence)
                 .randomize(parentCategoryPredicate, () -> null)
-                .randomize(childCategoriesPredicate, List::of)
-                .randomize(postsPredicate, List::of);
+                .randomize(childCategoriesPredicate, ArrayList::new)
+                .randomize(postsPredicate, ArrayList::new);
+
+        return new EasyRandom(param).nextObject(Category.class);
+    }
+
+    public static Category create(String title, int depth, int sequence, Category parentCategory) {
+        var idPredicate = named("id")
+                .and(ofType(Long.class))
+                .and(inClass(Category.class));
+
+        var titlePredicate = named("title")
+                .and(ofType(String.class))
+                .and(inClass(Category.class));
+
+        var depthPredicate = named("depth")
+                .and(ofType(int.class))
+                .and(inClass(Category.class));
+
+        var sequencePredicate = named("sequence")
+                .and(ofType(int.class))
+                .and(inClass(Category.class));
+
+        var parentCategoryPredicate = named("parentCategory")
+                .and(ofType(Category.class))
+                .and(inClass(Category.class));
+
+        var childCategoriesPredicate = named("childCategories")
+                .and(ofType(List.class))
+                .and(inClass(Category.class));
+
+        var postsPredicate = named("posts")
+                .and(ofType(List.class))
+                .and(inClass(Category.class));
+
+        var param = new EasyRandomParameters()
+                .randomize(idPredicate, () -> null)
+                .randomize(titlePredicate, () -> title)
+                .randomize(depthPredicate, () -> depth)
+                .randomize(sequencePredicate, () -> sequence)
+                .randomize(parentCategoryPredicate, () -> parentCategory)
+                .randomize(childCategoriesPredicate, ArrayList::new)
+                .randomize(postsPredicate, ArrayList::new);
 
         return new EasyRandom(param).nextObject(Category.class);
     }
